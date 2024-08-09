5a09a76b631c815e2b38eb4127b3d724ff71ac38
Thiago Dias da Silva <silvathiago011@hotmail.com>
2024-08-09 03:36:28 -0300

    b

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -2,23 +2,39 @@ import sendEmail from '../sendEmail';
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
-        const {
-            recipientEmail,
-            postalCode,
-            createdAt,
-            number,
-            district,
-            transactionId,
-            givenName,
-            amount,
-            cityUF,
-        } = req.body;
-
-        if (!recipientEmail) {
-            return res.status(400).json({ message: 'Recipient email is required' });
-        }
-
         try {
+            // Tentativa de leitura dos dados do corpo da requisição
+            const {
+                recipientEmail,
+                postalCode,
+                createdAt,
+                number,
+                district,
+                transactionId,
+                givenName,
+                amount,
+                cityUF,
+            } = req.body;
+
+            // Verificação básica dos parâmetros
+            if (!recipientEmail) {
+                return res.status(400).json({ message: 'Recipient email is required' });
+            }
+
+            // Debugging: Log dos parâmetros recebidos
+            console.log('Parameters received:', {
+                recipientEmail,
+                postalCode,
+                createdAt,
+                number,
+                district,
+                transactionId,
+                givenName,
+                amount,
+                cityUF,
+            });
+
+            // Chamada à função para enviar o email
             await sendEmail({
                 recipientEmail,
                 postalCode,
@@ -32,6 +48,7 @@ export default async function handler(req, res) {
             });
             res.status(200).json({ message: 'Email sent successfully' });
         } catch (error) {
+            console.error('Error processing request:', error);
             res.status(500).json({ message: 'Error sending email', error: error.message });
         }
     } else {
